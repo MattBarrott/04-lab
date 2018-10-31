6ae10704896f4689b93dd20877554d0fb57e0152
Matthew Barrott <P15080280@LEC-ADMIN.dmu.ac.uk>
2018-10-31 10:49:57 +0000

    Added javascript to listen for click on mobile menu.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 console.log("hello");
+
+const menuToggle = document.getElementById("menu-toggle");
+const menuNav = document.getElementById("menu-nav");
+
+const toggleMenu = () => {console.log("called toggleMenu")};
+menuToggle.addEventListener("click", toggleMenu);
